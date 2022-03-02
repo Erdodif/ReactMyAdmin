176801eefdab1a6ba596fcdc5f086e7161f0be51
Erdodif <erdodif@gmail.com>
2022-03-03 00:08:43 +0100

    tailing spaces and line-brakers

--- server.js
+++ server.js
@@ -15,31 +15,24 @@ var conn = mysql.createConnection({
     password: ""
 });
 
-conn.connect(function(err){
-    if(err)throw err;
+conn.connect(function (err) {
+    if (err) throw err;
     console.log("Database connected");
 })
 
 app.get('/', (req, res) => {
     try {
         console.log("get incoming");
-        conn.query("SHOW DATABASES",(err,result)=>{
-            if(err) return res.status(500).json({error:err});
+        conn.query("SHOW DATABASES", (err, result) => {
+            if (err) return res.status(500).json({ error: err });
             return res.status(200).json(result);
         });
     }
     catch (e) {
         console.error(e);
-        return res.status(500).json({error:'There might be a problem'});
+        return res.status(500).json({ error: 'There might be a problem' });
     }
 
-
-
-
-
-
-
-    
 });
 
 const PORT = process.env.PORT || 8080;
