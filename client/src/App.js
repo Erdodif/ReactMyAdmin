01b9b76aff279febf4f515bc141b034878d5b34b
Erdodif <erdodif@gmail.com>
2022-03-04 16:01:41 +0100

    Introduce new imports

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,13 @@
 import './App.scss';
+import Sidebar from './Sidebar';
+require('dotenv').config({path:"../.env"});
+const serverUrl = process.env.SERVER_HOST+":"+process.env.SERVER_PORT;
 
 function App() {
   return (
     <div className="App">
       <p>ReactMyAdmin</p>
+      <Sidebar url={serverUrl} />
     </div>
   );
 }
