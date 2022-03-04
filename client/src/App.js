764cf00d982816bab98f59a095c3bddb39e0a0a4
Erdodif <erdodif@gmail.com>
2022-03-05 00:04:45 +0100

    Move the app's render method to class

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,23 @@
+import './index.scss';
 import './App.scss';
 import Sidebar from './Sidebar';
+import React from 'react';
 const serverUrl = "http://"+process.env.REACT_APP_SERVER_HOST+":"+process.env.REACT_APP_SERVER_PORT;
 
-function App() {
-  return (
-    <div className="App">
-      <p>ReactMyAdmin</p>
-      <Sidebar url={serverUrl} />
-    </div>
-  );
+class App extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {theme:2};
+  }
+
+  render(){
+    return (
+      <div className={"App theme" + this.state.theme}>
+        <Sidebar url={serverUrl}/>
+      </div>
+    );
+  }
+  
 }
 
 export default App;
