cf7a65a9c71a6df5e67c299344264ae3c440ecd5
Erdodif <erdodif@gmail.com>
2022-03-15 15:17:56 +0100

    Templating

---
+++ client/src/Navigation.js
@@ -0,0 +1,10 @@
+import './Navigation.scss';
+import React from 'react';
+
+export default class Navigation extends React.Component{
+    render(){
+        return (
+            <div className='Navigation'>NavBar</div>
+        );
+    }
+}
