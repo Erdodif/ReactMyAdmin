3b1340350f8350eeb2d041c481458aebf7fb5430
Erdodif <erdodif@gmail.com>
2022-03-17 00:35:22 +0100

    Implement sidebar visibility toggle

--- client/src/Sidebar.js
+++ client/src/Sidebar.js
@@ -46,15 +46,36 @@ export default class Sidebar extends React.Component {
         });
     }
 
+    hideSidebar() {
+        let app = document.querySelector("#App");
+        let state = document.getElementById("hide-sidebar").checked;
+        let sidebar = document.querySelector(".Sidebar");
+        if(state){
+            app.classList.add("noSidebar");
+            sidebar.classList.add("hidden");
+        }
+        else{
+            app.classList.remove("noSidebar");
+            sidebar.classList.remove("hidden");
+        }
+        
+    }
+
     render() {
         return (
-            <div className="Sidebar">
-                <img className="Logo" src="reactmyadmin_logo_text.svg" alt="ReactMyAdmin" />
-                <div className="Searchbar" key={"Searchbar"}>
-                    <input type="text" />
-                </div>
-                <div className="Databases" key={"Databases"}>
-                    {this.state.databases.map((database) => <Database url={this.props.url} syncTable={(database,table)=>this.props.syncTable(database,table)} name={database.label} key={database.label} />)}
+            <div className="Sidebar" >
+                <input type="checkbox" name="hide-sidebar" id="hide-sidebar" onChange={this.hideSidebar} />
+                <label htmlFor="hide-sidebar" id="sidebar-button">
+                    <img src="arrow_up.ico" alt="" />
+                </label>
+                <div id="Sidebar">
+                    <img className="Logo" src="reactmyadmin_logo_text.svg" alt="ReactMyAdmin" />
+                    <div className="Searchbar" key={"Searchbar"}>
+                        <input type="text" />
+                    </div>
+                    <div className="Databases" key={"Databases"}>
+                        {this.state.databases.map((database) => <Database url={this.props.url} syncTable={(database, table) => this.props.syncTable(database, table)} name={database.label} key={database.label} />)}
+                    </div>
                 </div>
             </div>
         );
@@ -81,7 +102,7 @@ class Database extends React.Component {
                     <img src="arrow_up.ico" alt="" />
                     {name}
                 </div>
-                <Tables url={url} key={"Tables of " + name} syncTable={(table)=>this.props.syncTable(name,table)} selected={this.state.selected} />
+                <Tables url={url} key={"Tables of " + name} syncTable={(table) => this.props.syncTable(name, table)} selected={this.state.selected} />
             </div>
         );
     }
@@ -120,14 +141,14 @@ class Tables extends React.Component {
                 console.error(error);
                 alert(error);
             });
-       }
+        }
     }
 
     render() {
         let tables = null;
         if (this.state.tables !== null && this.props.selected) {
             tables = this.state.tables.map(
-                (table) => <Table name={table.name} key={"Table " + table.name} syncTable={this.props.syncTable}/>
+                (table) => <Table name={table.name} key={"Table " + table.name} syncTable={this.props.syncTable} />
             );
         }
         return (
@@ -147,7 +168,7 @@ class Table extends React.Component {
     render() {
         const { name, selected } = this.props;
         return (
-            <div className={selected ? "Table" : "Table selected"} onClick={()=>{this.props.syncTable(name);console.log(name);}}>
+            <div className={selected ? "Table" : "Table selected"} onClick={() => { this.props.syncTable(name); console.log(name); }}>
                 {name}
             </div>
         );
